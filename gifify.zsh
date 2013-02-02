7953247e0cd35d2560f52e475ca3fbce7d5e1110
Petrus Repo <petrus.repo@iki.fi>
2013-02-02 11:58:26 +0200

    Replaced Imagemagick with gifsicle

--- gifify.zsh
+++ gifify.zsh
@@ -1,32 +1,42 @@
-# Convert a Quick Time screencast to GIF
-# Creates two gifs, one HUGE and one optimized.
+# Convert a Quick Time screencast to gif.
+# NOTE: In Quick Time, use the export to "iPod touch & iPhone 3GS"
+#       option. This will create a .m4v file which results in a
+#       better gif than a .mov file.
 #
 # Dependencies:
-#  % brew install ffmpeg imagemagick
+#  % brew install ffmpeg gifsicle
 #
 # Props to http://schneems.com/post/41104255619/use-gifs-in-your-pull-request-for-good-not-evil
+#      and http://www.reddit.com/r/ruby/comments/16zvjt/use_gifs_in_your_pull_request_for_good_not_evil/c81btvm
 function gifify {
-        SRC="$1"
-        DEST="$2"
-        GIF="${DEST}.gif"
-        GIF_HUGE="${DEST}-HUGE.gif"
+        src="$1"
+        gif="$2"
 
-        if [ "$SRC" = "" ] || [ "$DEST" = "" ]; then
-                echo "Usage: $0 [source] [dest]"
-                echo "Converts [source] to gif file [dest]"
+        if [ "$src" = "" ] || [ "$gif" = "" ]; then
+                echo "Usage: $0 [source file] [target.gif]"
+                echo "Converts [source file] to gif file [target.gif]"
                 return
         fi
 
-        echo "Will create ${GIF} and ${GIF_HUGE} from ${SRC}."
+        echo "Will create ${gif} from ${src}."
         echo -n "Ok? [y/n] "
-        read OK
+        read ok
 
-        if [ $OK != "y" ]; then
+        if [ $ok != "y" ]; then
                 return
         fi
 
-        ffmpeg -i "${SRC}" -pix_fmt rgb24 "${GIF_HUGE}"
-        convert -layers Optimize "${GIF_HUGE}" "${GIF}"
+        gifify_tmpdir=`mktemp -d ${TMPDIR-/tmp}/gifify.$$` || return
+        orig_dir=`pwd`
 
-        echo "Created ${GIF}, you may remove the HUGE ${GIF_HUGE}."
+        cd ${gifify_tmpdir} && \
+        ffmpeg  -loglevel quiet -i "${orig_dir}/${src}" ${gifify_tmpdir}/%d.png && \
+        for i in ${gifify_tmpdir}/*.png ; do convert $i $i.gif ; done && \
+        gifsicle --colors 256 -O3 -Okeep-empty -d2 -l $(ls *.png.gif |sort -g) > ${orig_dir}/$gif
+
+        cd ${orig_dir}
+
+        rm ${gifify_tmpdir}/*.{png.gif,png} && rmdir ${gifify_tmpdir}
+
+        echo "Created ${gif}."
 }
